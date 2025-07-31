9c0154639ebcb9dd2dabc44fa3b25191d9725c5b
Mateo06pppp <henaom0002@gmail.com>
2025-07-31 11:25:30 -0500

    cambios en plan maestro, plan semanal y plan color

--- dto/PlanMaestro.ts
+++ dto/PlanMaestro.ts
@@ -1,12 +1,6 @@
-export interface PlanMaestroColorDTO {
-  color: string;
-  cantidad: number;
-}
-
 export interface PlanMaestroDTO {
   mes: string;
   id_producto: number;
   cantidad_objetivo: number;
   cantidad_produccion: number;
-  colores: PlanMaestroColorDTO[];
 }
